09231d4402aec469d3d15793eb4eac187519d60f
User <User@WIN-R1VEPF5FUNI>
2019-03-09 22:37:34 +0300

    initial commit

---
+++ task_DEV1/task_DEV1/EntryPoint.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace task_DEV1
+{
+    class EntryPoint
+    {
+        /// <summary>
+        /// Entry point
+        /// </summary>
+        /// <param name="args">Arguments from command line</param>
+        static void Main(string[] args)
+        {
+            try
+            {
+                UniqueSymbols uniquesybols = new UniqueSymbols();                
+                uniquesybols.DisplaySubsequence(uniquesybols.SubsequenceFinder(args[0]));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error" + ex.Message);
+            }
+        }
+    }
+}
+
