87ccbc8c00d066de86c152fa28cd56d6bc2f25d6
User <User@WIN-R1VEPF5FUNI>
2019-04-18 17:02:23 +0300

    modified comments

---
+++ task_DEV6/AddingToCarList.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace Task_DEV5
+{
+    /// <summary>
+    /// Class AddingToCarList for repeatyng input cars in list.
+    /// </summary>
+    class AddingToCarList
+    {
+        /// <summary>
+        /// Method AddingCarsToList repeat request to add cars.
+        /// </summary>
+        public CarList AddingCarsToList()
+        {
+            List<Car> listOfCars = new List<Car>();
+            CarList carList = new CarList();
+            Car car = new Car();
+            carList.AddToCarList(car, listOfCars);
+            return carList;
+        }
+    }
+}
---
+++ task_DEV6/CarList.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_DEV5
+{
+    /// <summary>
+    /// Class CarList for making list from cars.
+    /// </summary>
+    class CarList
+    {
+        /// <summary>
+        /// Method AddToCarlist checks for correct input and adds cars in list.
+        /// </summary>
+        /// <returns>List of cars</returns>
+        public List<Car> listOfCar = new List<Car>();
+        public List<Car> AddToCarList(Car car, List<Car> listOfCars)
+        {
+            if (listOfCars.Count == 0)
+            {
+                listOfCars.Add(car);
+            }
+            else
+            {
+                foreach (Car c in listOfCars)
+                {
+                    if (c.brand == car.brand && c.model == car.model && c.price == car.price)
+                    {
+                        c.quantity += car.quantity;
+                        break;
+                    }
+                    else if (c.brand == car.brand && c.model == car.model && c.price != car.price)
+                    {
+                        Console.WriteLine("incorrect input");
+                        break;
+                    }
+                    else
+                    {
+                        listOfCars.Add(car);
+                        break;
+                    }
+                }
+            }
+            return listOfCar = listOfCars; //return list of cars
+        }
+    }
+}
+
