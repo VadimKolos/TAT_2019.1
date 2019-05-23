5ce726c930b890169751dec26021432785cc5057
User <User@WIN-R1VEPF5FUNI>
2019-05-23 14:20:28 +0300

    first commit

--- Task_DEV10/Properties/AssemblyInfo.cs
+++ Task_DEV10/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("task_DEV2")]
+[assembly: AssemblyTitle("Task_DEV10")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("task_DEV2")]
+[assembly: AssemblyProduct("Task_DEV10")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
-[assembly: Guid("02ae11af-4a43-4dbb-82e0-44c370f58964")]
+[assembly: Guid("74102690-9434-4b24-b92e-b7286a7ae0be")]
 
 // Сведения о версии сборки состоят из следующих четырех значений:
 //
