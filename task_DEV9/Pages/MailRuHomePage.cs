dc1f9c23668d7cef9292b433874cb79fcdef2e00
User <User@WIN-R1VEPF5FUNI>
2019-08-02 01:18:03 +0300

    add corrected tests

---
+++ task_DEV9/Pages/MailRuHomePage.cs
@@ -0,0 +1,44 @@
+﻿using OpenQA.Selenium;
+using System.Collections.ObjectModel;
+using task_DEV9.WebDriver;
+
+namespace task_DEV9.Pages
+{
+    /// <summary>
+    /// This class works with home page of MailRu.
+    /// </summary>
+    public class MailRuHomePage : BasePage
+    {
+        public MailRuHomePage(IWebDriver driver) : base(driver)
+        {
+        }
+
+        private IWebElement messageButton => driver.FindElement(By.XPath("//span[text()='Написать письмо']"));
+        private IWebElement sendedMessage => driver.FindElement(By.XPath("//div[text()='fake Account']"));
+        private IWebElement unreadedReceivedMessage => driver.FindElement(By.XPath("//div[text()='fake Account']"));
+
+        ReadOnlyCollection<IWebElement> unreadMessages => driver.FindElements(By.XPath("//div[@class='b-letterstatus__icon ico " +
+                "ico_letterstatus ico_letterstatus_onhover ico_letterstatus_unread']"));
+
+        public void ReadMessage()
+        {
+            sendedMessage.Click();
+        }
+
+        public bool HomePageIsOpened()
+        {
+            return messageButton.Displayed;
+        }
+
+        public bool MessageIsSended()
+        {
+            return sendedMessage.Displayed;
+        }
+
+        public bool MessageIsUnread()
+        {
+            return unreadedReceivedMessage.Displayed;
+        }
+        
+    }
+}
---
+++ task_DEV9/WebDriver/BaseTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+
+namespace task_DEV9.WebDriver
+{
+    /// <summary>   
+    /// Base class for tests.
+    /// </summary>
+    public class BaseTest
+    {
+        protected static IWebDriver driver;
+
+        /// <summary>        
+        /// This method setup driver for test
+        /// </summary>
+        [SetUp]
+        public virtual void InitTest()
+        {
+            driver = new ChromeDriver("C://SeleniumDriver");
+            driver.Manage().Window.Maximize();                       
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+        }
+
+        public void GoToUrl(string mailURL)
+        {
+            driver.Navigate().GoToUrl(mailURL);
+        }
+
+        /// <summary>
+        ///This method quit from driver after test
+        /// </summary>
+        [TearDown]
+        public virtual void CleanTest()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
