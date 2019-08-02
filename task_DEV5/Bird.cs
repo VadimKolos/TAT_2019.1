ca444b55efe21ea956688c14d8a77a6d3c24e65b
User <User@WIN-R1VEPF5FUNI>
2019-08-02 13:01:29 +0300

    cosmetic changes

---
+++ task_DEV5/Bird.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace task_DEV5
+{
+    /// <summary>
+    /// This class work with parametres of bird.
+    /// </summary>
+    class Bird : IFlyable
+    {
+        Point point;
+
+        /// <summary>
+        /// Final coordinates of bird.
+        /// </summary>
+        public int newCoordinateX { get; set; }
+        public int newCoordinateY { get; set; }
+        public int newCoordinateZ { get; set; }
+
+        /// <summary>
+        /// This method returns final point of bird.
+        /// </summary>
+        /// <param name="newPoint">final point</param>
+        /// <param name="endPoint">Array of x, y, z - parametres of final point</param>
+        public int[] FlyTo(Point newPoint)
+        {
+            newCoordinateX = newPoint.coordinateX;
+            newCoordinateY = newPoint.coordinateY;
+            newCoordinateZ = newPoint.coordinateZ;
+            int[] endPoint = { newCoordinateX, newCoordinateY, newCoordinateZ };
+            return endPoint;
+        }
+
+        /// <summary>
+        /// This method returns name of this object.
+        /// </summary>
+        public string WhoAmI()
+        {
+            return "Bird";
+        }
+
+        /// <summary>
+        /// This method calculates flight time of the bird, whoose start speed calculate randomly between 1-20 km/h.        
+        /// </summary>        
+        /// <param name="endPoint">Array of x, y, z - parametres of final point</param>
+        /// <param name="distance">Distance between start point and finite point</param>
+        /// <param name="timeOfFlying">time of flying of the bird</param>
+        public double GetFlyTime(int[] endPoint)
+        {
+            Random random = new Random();
+            int minSpeed = 1;
+            int maxSpeed = 20;
+            int speed = random.Next(minSpeed, maxSpeed);
+            var timeOfFlying = Math.Sqrt((newCoordinateX - point.coordinateX) ^ 2 + (newCoordinateY - point.coordinateY) ^ 2
+                + (newCoordinateZ - point.coordinateZ)) / speed;
+
+            return timeOfFlying;
+        }
+    }
+}
