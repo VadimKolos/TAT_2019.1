c12459dfec0b9ebaeae5df94610f9cbc2b426ee4
User <User@WIN-R1VEPF5FUNI>
2019-08-02 14:31:38 +0300

    resolved conflicts

---
+++ taskDEV3/DisplayerScreen.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace taskDEV3
+{
+    /// <summary>
+    /// This class shows information about employee by criterion.
+    /// </summary>
+    public class DisplayerScreen
+    {
+        public void ShowFirstCriterion(int[] employeeArray)
+        {
+            Console.WriteLine("Count of juniors: " + employeeArray[0]);
+            Console.WriteLine("Count of middles: " + employeeArray[1]);
+            Console.WriteLine("Count of seniors: " + employeeArray[2]);
+            Console.WriteLine("Count of leads: " + employeeArray[3]);
+            Console.WriteLine("Maximum productivity: " + employeeArray[4]);
+        }
+
+        public void ShowSecondCriterion(int[] employeeArray)
+        {
+            Console.WriteLine("Count of juniors: " + employeeArray[0]);
+            Console.WriteLine("Count of middles: " + employeeArray[1]);
+            Console.WriteLine("Count of seniors: " + employeeArray[2]);
+            Console.WriteLine("Count of leads: " + employeeArray[3]);
+            Console.WriteLine("Minimum total salary: " + employeeArray[4]);
+        }
+
+        public void ShowThirdCriterion(int[] employeeArray)
+        {
+            Console.WriteLine("Count of middles: " + employeeArray[0]);
+            Console.WriteLine("Count of seniors: " + employeeArray[1]);
+            Console.WriteLine("Count of leads: " + employeeArray[2]);
+            Console.WriteLine("Minimum count of high qualified employees: " + employeeArray[3]);
+        }
+    }
+}
+
---
+++ taskDEV3/EntryPoint.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace taskDEV3
+{
+    /// <summary>
+    /// Entry point
+    /// </summary>    
+    class EntryPoint
+    {
+        /// <summary>
+        /// Entry point
+        /// </summary>
+        /// <param name="args">Arguments from command line</param>
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args[0] == string.Empty || args[1] == string.Empty || args[2] == string.Empty)
+                {
+                    Console.WriteLine("Incorreсt input:  use 3 parametres");
+                    Environment.Exit(1);
+                }
+                else
+                {
+                    TeamCreator optimizer = new TeamCreator(args[0], args[1], args[2]);
+                    optimizer.InputDataTake();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error " + ex.Message);
+            }
+        }
+    }
+}
+
